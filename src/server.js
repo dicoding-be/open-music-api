799c377812a385936914a2d7fac65639a3dc1f53
Rizal Fahmi <rizalfahmi3002@gmail.com>
2024-01-29 22:40:26 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const registerPlugins = require('./registerPlugins');
 const setupErrorHandling = require('./errorHandling');
 
@@ -13,6 +14,30 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  // register plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategi autentikasi jwt
+  server.auth.strategy('openmusic_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await registerPlugins(server);
